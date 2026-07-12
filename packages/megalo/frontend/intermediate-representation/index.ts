d3f1ba21cf6289f3d49496e2276594ca4ac45904
Codie Newark <git@codie.gg>
2026-07-12 17:35:56 +0900

    ValueWithLocation type check

--- packages/megalo/frontend/intermediate-representation/index.ts
+++ packages/megalo/frontend/intermediate-representation/index.ts
@@ -1,9 +1,15 @@
 import { SourceLocation } from "../diagnostics";
 import { GameEngineCustomVariant } from "./game/game_variant";
 
-export type ValueWithLocation<T> = T & {
-    location: SourceLocation;
-}
+type LocationMeta = { location: SourceLocation };
+export type ValueWithLocation<T> = 
+    // Enforce that the provided T doesnt already have a location property.
+    // Then enforce that a SourceLocation is provided.
+    T extends object 
+        ? "location" extends keyof T
+            ? never
+            : T & LocationMeta
+        : T & LocationMeta;
 
 export type IR = {
     game_variant: GameEngineCustomVariant;
